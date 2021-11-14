f803a953eac46e5a93298b9c2cf94d7b05963302
dantt <tongthidan99@gmail.com>
2021-11-14 23:48:27 +0700

    init commit

---
+++ handle-data-set/src/main/java/com/example/handledataset/HandleDataSetApplication.java
@@ -0,0 +1,13 @@
+package com.example.handledataset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleDataSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleDataSetApplication.class, args);
+	}
+
+}
